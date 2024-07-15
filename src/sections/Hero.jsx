fb478270e3f67fc75712ac39c30ea75360669f83
Dhyaneshwar N <dhyaneshwar98@gmail.com>
2024-07-15 11:09:44 +0100

    feat: adding transition for nike word

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -18,7 +18,7 @@ const Hero = () => {
             The New Arrival
           </span>
           <br />
-          <span className="relative inline-block transition-all hover:before:absolute hover:before:-inset-0 hover:before:-bottom-3 hover:before:z-20 hover:before:block hover:before:-skew-y-3 hover:before:bg-pink-500">
+          <span className="relative inline-block transition-all before:transition-all before:duration-700 hover:before:absolute hover:before:-inset-0 hover:before:-bottom-3 hover:before:z-20 hover:before:block hover:before:-skew-y-3 hover:before:bg-pink-500">
             <span className="relative z-30 mt-3 inline-block animate-bounce text-coral-red hover:text-white">
               Nike
             </span>
